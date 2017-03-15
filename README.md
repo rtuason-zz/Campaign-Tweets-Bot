06baadb0b205358e81981e2cacec60f9724c781d
Ramon Tuason <Aether@Ramons-MacBook-Pro.local>
2017-03-14 22:05:11 -0700

    Fixed bugs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Campaign-Tweets-Bot
+Hillary won the popular vote by about three million votes, but how did her popularity fare against Donald Trump's during their campaigns? A dataset on Kaggle (https://www.kaggle.com/benhamner/clinton-trump-tweets) contains thousands of tweets from both presidential candidates during mid-2016, and my bot allows us to examine how popular each one's tweets were given a range of dates.
+
+Using a range of dates, the bot will look at each candidate's total number of tweets, and more importantly, the average tweet's retweet and favorite counts.
+
+Flags:
+--dates
+Provide two dates between 4/17/16 and 9/29/16, which is the range in which both datasets contain tweets from both candidates. If this flag is not used, the bot will give info on the entire range.
+
+--phone
+Provide as many phone numbers as you'd like, barring AWS cost constraints. Each one will receive the info described above
+
+--twitter
+Similar to the phone flag above. Don't include the '@' sign.
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from os.path import exists
 import argparse
 
 import csv
+import json
 import requests
 import re
 import calendar
@@ -40,15 +41,15 @@ def get_tweet_stats(tweets):
     retweet_sum += int(tweet['retweet_count'])
     favorite_sum += int(tweet['favorite_count'])
 
-  retweet_avg = round(retweet_sum / len(tweets), 2)
-  favorite_avg = round(favorite_sum / len(tweets), 2)
+  retweet_avg = "{:,.2f}".format(round(retweet_sum / len(tweets), 2))
+  favorite_avg = "{:,.2f}".format(round(favorite_sum / len(tweets), 2))
+  num_tweets = format(len(tweets), ',d')
 
-  return retweet_avg, favorite_avg, len(tweets)
+  return num_tweets, retweet_avg, favorite_avg
 
 ######################################################################
 # Tweet functions for a range of dates
 ######################################################################
-
 def filter_tweets(tweets, handle, date1, date2):
   results = []
   for tweet in tweets:
@@ -74,27 +75,45 @@ def compare_tweets(date1, date2):
 # Formulate a response
 ######################################################################
 def create_response(date1, date2, clinton_stats, trump_stats):
-  output = "From {date1} to {date2}, Hillary Clinton had averages of {HC_retweet_avg} retweets and {HC_favorite_avg} favorites from {HC_num_tweets} tweets, while Donald Trump had averages of {DT_retweet_avg} retweets and {DT_favorite_avg} favorites from {DT_num_tweets} tweets."
+  output = "From {date1} to {date2}, Hillary Clinton's {HC_num_tweets} tweets averaged {HC_retweet_avg} retweets and {HC_favorite_avg} favorites, while Donald Trump's {DT_num_tweets} tweets averaged {DT_retweet_avg} retweets and {DT_favorite_avg} favorites."
   output = output.format(
-    date1=date1,
-    date2=date2,
-    HC_retweet_avg=clinton_stats[0],
-    HC_favorite_avg=clinton_stats[1],
-    HC_num_tweets=clinton_stats[2],
-    DT_retweet_avg=trump_stats[0],
-    DT_favorite_avg=trump_stats[1],
-    DT_num_tweets=trump_stats[2]
+    date1=date1.strftime("%b %d, %Y"),
+    date2=date2.strftime("%b %d, %Y"),
+    HC_num_tweets=clinton_stats[0],
+    HC_retweet_avg=clinton_stats[1],
+    HC_favorite_avg=clinton_stats[2],
+    DT_num_tweets=trump_stats[0],
+    DT_retweet_avg=trump_stats[1],
+    DT_favorite_avg=trump_stats[2]
   )
 
   return output
 
+def create_twitter_response(date1, date2, clinton_stats, trump_stats):
+  output1 = "From {date1} to {date2}, Hillary Clinton's {HC_num_tweets} tweets averaged {HC_retweet_avg} retweets and {HC_favorite_avg} favorites, while..."
+  output2 = "Donald Trump's {DT_num_tweets} tweets averaged {DT_retweet_avg} retweets and {DT_favorite_avg} favorites."
+  
+  output1 = output1.format(
+    date1=date1.strftime("%b %d, %Y"),
+    date2=date2.strftime("%b %d, %Y"),
+    HC_num_tweets=clinton_stats[0],
+    HC_retweet_avg=clinton_stats[1],
+    HC_favorite_avg=clinton_stats[2]
+  )
+  output2 = output2.format(
+    DT_num_tweets=trump_stats[0],
+    DT_retweet_avg=trump_stats[1],
+    DT_favorite_avg=trump_stats[2]
+  )
+
+  return output1, output2
+
 ######################################################################
 # Analyze user input arguments
 ######################################################################
-
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument("--date", nargs="*")
+  parser.add_argument("--dates", nargs="*")
   parser.add_argument("--phone", nargs="*")
   parser.add_argument("--twitter", nargs="*")
   args = parser.parse_args()
@@ -102,16 +121,16 @@ if __name__ == '__main__':
   min_date = dateparser.parse(MIN_DATE)
   max_date = dateparser.parse(MAX_DATE)
 
-  if args.date:
+  if args.dates:
     date_pattern = re.compile("^[4-9]/\d{1,2}/16$")
     
-    if len(args.date) == 2 and date_pattern.match(args.date[1]) and date_pattern.match(args.date[2]):
+    if len(args.dates) == 2 and date_pattern.match(args.dates[0]) and date_pattern.match(args.dates[1]):
       try:
-        temp1 = dateparser.parse(args.date[1])
-        temp2 = dateparser.parse(args.date[2])
-      else:
+        temp1 = dateparser.parse(args.dates[0])
+        temp2 = dateparser.parse(args.dates[1])
+      except:
         print("Invalid date(s) provided. They must also follow the format m/dd/yy.")
-        return
+        exit()
 
       date1 = min(temp1, temp2)
       date2 = max(temp1, temp2)
@@ -121,13 +140,14 @@ if __name__ == '__main__':
         response = create_response(date1, date2, clinton_stats, trump_stats)
       else:
         print("Your date range must fall between 4/17/16 and 9/29/16, inclusive")
-        return
+        exit()
     else:
-      print("If using dates, exactly two must be provided.")
-      return
+      print("If using dates, you must provide exactly two that follow the format 'm/dd/yy'.")
+      exit()
   else:
     clinton_stats, trump_stats = compare_tweets(min_date, max_date)
-    response = create_response(min_date, max_date, clinton_stats, trump_stats)
+    date1, date2 = min_date, max_date
+    response = create_response(date1, date2, clinton_stats, trump_stats)
 
   print(response + '\n')
 
@@ -138,13 +158,13 @@ if __name__ == '__main__':
       if not phone_pattern.match(phone_number):
         print("At least one phone number has been formatted incorrectly. Phone numbers must start with '+1', followed by ten numbers.")
         numbers_correct = False
-        break
+        break   # Don't exit. There might be Twitter handles.
 
     if numbers_correct:
       session = boto3.Session(profile_name='default')
       sns = session.client('sns')
       for phone_number in args.phone:
-        # sns.publish(PhoneNumber=phone_number, Message=response)
+        sns.publish(PhoneNumber=phone_number, Message=response)
 
   if args.twitter:
     twitter_pattern = re.compile("^[\w]{1,15}$")
@@ -160,6 +180,11 @@ if __name__ == '__main__':
         creds = json.load(f)
 
       client = Twython(creds['consumer_key'], creds['consumer_secret'], creds['access_token'], creds['access_token_secret'])
+
+      response1, response2 = create_twitter_response(date1, date2, clinton_stats, trump_stats)
+
       for handle in args.twitter:
-        msg = "@{} {}".format(handle, response)
-        client.update_status(status=msg)
+        msg1 = "@{} {}".format(handle, response1)
+        msg2 = "@{} {}".format(handle, response2)
+        client.update_status(status=msg1)
+        client.update_status(status=msg2)
